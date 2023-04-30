9d378354520e413428431021cae77d781d452827
ttmtu2003 <ttmtu2003@gmail.com>
2023-04-30 11:03:36 -0700

    update

--- flask-backend/app/__init__.py
+++ flask-backend/app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for, redirect, request
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 from config import Config
 import os
@@ -8,15 +10,21 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
+
 
 app.config['SESSION_COOKIE_DOMAIN'] = 'localhost.localdomain' # Set the session cookie domain
-
 app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 #app.secret_key = os.getenv("SECRET_KEY")
 #app.secret_key = 'youcantbrokeit'
 app.config['SERVER_NAME'] = 'localhost:5000'
+
+# Initialize Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 app.secret_key = "secret key"
 from app.routes import *
--- flask-backend/app/models.py
+++ flask-backend/app/models.py
@@ -1,15 +1,34 @@
 from app import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     """
-    User class, which contains id, username, password, email and the products.
+    User class, which contains id, username, password, fullname and the products.
     """
     id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.Integer)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
+    password_hash = db.Column(db.String(20))
+    full_name = db.Column(db.String(100), nullable=True)
+    active = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+    @property
+    def is_active(self):
+        return self.active
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password, method='scrypt')
+
+    def verify_password(self, password):
+        if self.password_hash is None:
+            return False
+        return check_password_hash(self.password_hash, password)
