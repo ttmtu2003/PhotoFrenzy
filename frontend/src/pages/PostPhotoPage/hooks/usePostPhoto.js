33b754fb25123b3387c2887345045108df655abf
ttmtu2003 <ttmtu2003@gmail.com>
2023-05-12 22:55:48 -0700

    merge

--- frontend/src/pages/PostPhotoPage/hooks/usePostPhoto.js
+++ frontend/src/pages/PostPhotoPage/hooks/usePostPhoto.js
@@ -16,7 +16,7 @@ const usePostPhoto = () => {
       });
       setResponse(response.data);
       setIsLoading(false);
-      window.location.href = '/explore'
+      window.location.href = '/profile'
     } catch (error) {
       setError(error);
       setIsLoading(false);
